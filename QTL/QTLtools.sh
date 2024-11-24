f8ee5838146465d46c3ec1142a68d176b5d911df
daigorohirohama <daigoro.hirohama@med.teikyo-u.ac.jp>
2024-11-24 17:27:54 +0900

    Update QTLtools.sh

--- QTL/QTLtools.sh
+++ QTL/QTLtools.sh
@@ -37,3 +37,17 @@ QTLtools cis \
 --std-err \
 --out /path/to/output_directory/cis_nominal_pQTL.txt
 
+
+###################################################################################
+#        #3: trans Nominal        
+###################################################################################
+
+module load QTLtools/1.3.1-11
+
+QTLtools trans \
+    --vcf /path/to/vcf/vcf_file.vcf.gz \
+    --bed /path/to/bed/bed_file.bed.gz \
+    --cov /path/to/covariate_file/cov_file.txt \
+    --nominal \
+    --threshold 1e-5 \
+    --out /path/to/output_directory/trans_nominal_pQTL.txt
